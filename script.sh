ad0abe39fbef588b77b87d901a884eb0ab97cd58
Eduard Gruy <eduard.gruy@datavard.com>
2020-10-08 16:55:25 +0200

    typo

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 az eventhubs namespace authorization-rule keys list \
  --resource-group $1 \
  --namespace-name $2 \
- --name RootManageSharedAccessKey > $AZ_SCRIPTS_OUTPUT_PATHs
+ --name RootManageSharedAccessKey > $AZ_SCRIPTS_OUTPUT_PATH
