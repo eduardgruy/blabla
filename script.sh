7895c451b6dc838b970bdcade66987b5c5ada793
Eduard Gruy <eduard.gruy@datavard.com>
2020-10-20 14:51:08 +0200

    removed backslash

--- script.sh
+++ script.sh
@@ -11,7 +11,8 @@ az functionapp deployment source config-zip --resource-group $rgname --name $fun
 wget -O 'logistics-app.zip' 'https://storgluedeployment.blob.core.windows.net/artifacts/logistics-app.zip?sp=r&st=2020-10-14T09:46:12Z&se=2022-10-14T17:46:12Z&spr=https&sv=2019-12-12&sr=b&sig=5kiJgiB0zV3F%2FHlhYNOSgYpjex%2Fzm8%2Fleywld%2FAwD2k%3D'
 
 az webapp deployment source config-zip --resource-group $rgname --name $webappname --src logistics-app.zip
-
+az webapp config set -n $webappname -g $rgname --startup-file='pm2 serve /home/site/wwwroot/build --no-daemon'
+az webapp restart -n $webappname -g $rgname
 
 # az functionapp deployment source config-zip -g test2 -n \
 # 'functtstdtjs3v2s6' --src ./functions.zip
