2bb7acab273d5354692f881987f3cf95da03f1e4
Eduard Gruy <eduard.gruy@datavard.com>
2020-11-03 11:32:45 +0100

    s

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
-rgname=$1
-functionappname=$2
-webappname=$3
+#!/bin/bash
+RG_NAME=$1
+FUNCTION_APP_NAME=$2
+WEB_APP_NAME=$3
+EVH_CONNECTION_STRING=$4
 
 
 get_sas_token() {
@@ -20,24 +21,24 @@ get_sas_token() {
     echo -n "SharedAccessSignature sr=$ENCODED_URI&sig=$ENCODED_HASH&se=$TTL&skn=$SHARED_ACCESS_KEY_NAME"
 }
 
-echo "$rgname $functionappname $webappname" 1>&2
+echo "$RG_NAME $FUNCTION_APP_NAME $WEB_APP_NAME" 1>&2
 wget -O 'functions.zip' 'https://storgluedeployment.blob.core.windows.net/artifacts/functions.zip?sp=r&st=2020-10-13T13:02:20Z&se=2022-10-13T21:02:20Z&spr=https&sv=2019-12-12&sr=b&sig=AZkZbkC6QV7d1Yr1MMHTo3IRx4KSKLzB8qemY6amWXQ%3D'
 
-az functionapp deployment source config-zip --resource-group $rgname --name $functionappname --src functions.zip
+az functionapp deployment source config-zip --resource-group $RG_NAME --name $FUNCTION_APP_NAME --src functions.zip
 
 wget -O 'logistics-app.zip' 'https://storgluedeployment.blob.core.windows.net/artifacts/logistics-app.zip?sp=r&st=2020-10-14T09:46:12Z&se=2022-10-14T17:46:12Z&spr=https&sv=2019-12-12&sr=b&sig=5kiJgiB0zV3F%2FHlhYNOSgYpjex%2Fzm8%2Fleywld%2FAwD2k%3D'
 
-az webapp deployment source config-zip --resource-group $rgname --name $webappname --src logistics-app.zip
-az webapp config set -n $webappname -g $rgname --startup-file='pm2 serve /home/site/wwwroot/build --no-daemon'
-az webapp restart -n $webappname -g $rgname
+az webapp deployment source config-zip --resource-group $RG_NAME --name $WEB_APP_NAME --src logistics-app.zip
+az webapp config set -n $WEB_APP_NAME -g $RG_NAME --startup-file='pm2 serve /home/site/wwwroot/build --no-daemon'
+az webapp restart -n $WEB_APP_NAME -g $RG_NAME
 
-ENDPOINT=${CONNECTION_STRING%/;*}
+ENDPOINT=${EVH_CONNECTION_STRING%/;*}
 URL=${ENDPOINT/"Endpoint=sb:"/"https:"}
 
-TMP_KEYNAME=${CONNECTION_STRING#*SharedAccessKeyName=}
+TMP_KEYNAME=${EVH_CONNECTION_STRING#*SharedAccessKeyName=}
 KEYNAME=${TMP_KEYNAME%%;*}
 
-TMP_KEY_VALUE=${CONNECTION_STRING#*SharedAccessKey=}
+TMP_KEY_VALUE=${EVH_CONNECTION_STRING#*SharedAccessKey=}
 KEY_VALUE=${TMP_KEY_VALUE%%;*}
 
 get_sas_token $URL $KEYNAME $KEY_VALUE | jq -c '{Result: map({id: .id})}' > output.txt
